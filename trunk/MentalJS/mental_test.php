458eca698f3e106f84efc23dad479e61a4782aac
gazheyes@gmail.com <gazheyes@gmail.com@417fb7ae-2eb8-e41c-4528-4155f6a87ef3>
2012-11-16 16:56:56 +0000

    x utf-8 support

--- trunk/MentalJS/mental_test.php
+++ trunk/MentalJS/mental_test.php
@@ -1,10 +1,14 @@
 <?php
 require("Mental.php");
 $input = (string) $_POST['input'];
+if(!$_POST['run']) {
+	$input = (string) $_GET['input'];
+}
 ?>
 <!doctype html>
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>MentalJS PHP test</title>
 		<style>
 			body {
@@ -31,7 +35,7 @@ $input = (string) $_POST['input'];
 		<legend>MentalJS PHP</legend>
 		<form action="mental_test.php" method="post">
 			<label>Input</label>
-			<textarea name="input" id="input"><?php echo htmlentities($input, ENT_QUOTES);?></textarea>			
+			<textarea name="input" id="input"><?php echo htmlentities($input, ENT_QUOTES, 'UTF-8');?></textarea>			
 			<?php IF($_POST['run']):?>			
 			<?php
 			$start = microtime(true);
@@ -44,7 +48,7 @@ $input = (string) $_POST['input'];
 						$result = $js->rewrite($input);
 					} catch (Exception $e) {
 					    $result = $e->getMessage();
-					}
+					}				
 				break;
 				case "Minify":
 					$js = new MentalJS;
@@ -52,7 +56,7 @@ $input = (string) $_POST['input'];
 						$result = $js->minify($input);
 					} catch (Exception $e) {
 					    $result = $e->getMessage();
-					}
+					}					
 				break;
 				case "isValid":
 					$js = new MentalJS;
@@ -78,16 +82,16 @@ $input = (string) $_POST['input'];
 			echo '<p><b>Parse time:' . abs($end - $start) . 'ms</b></p>';
 			?>
 			<label>Output</label>
-			<textarea name="output" id="output"><?php echo htmlentities($result, ENT_QUOTES);?></textarea>
+			<textarea name="output" id="output"><?php echo htmlentities($result, ENT_QUOTES, 'UTF-8');?></textarea>
 			<?php ENDIF?>
 			<fieldset>
 				<legend>Options</legend>
 				<select name="option">
 					<option value="">Please select</option>
-					<option>Rewrite</option>
-					<option>Minify</option>
-					<option>isValid</option>
-					<option>ParseTree</option>
+					<option<?php echo $_POST['option'] === 'Rewrite' ? ' selected="selected"' : ''?>>Rewrite</option>
+					<option<?php echo $_POST['option'] === 'Minify' ? ' selected="selected"' : ''?>>Minify</option>
+					<option<?php echo $_POST['option'] === 'isValid' ? ' selected="selected"' : ''?>>isValid</option>
+					<option<?php echo $_POST['option'] === 'ParseTree' ? ' selected="selected"' : ''?>>ParseTree</option>
 				</select>
 			</fieldset>
 			<input type="hidden" Value="1" name="run" />
